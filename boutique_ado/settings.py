e9a4281fad52f4066686f03c579cebc9dc22ce6e
Stuartpkd <stuartparkerdesign@gmail.com>
2023-10-10 14:22:27 +0000

    Update checkout form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from pathlib import Path
+# import dj_database_url
 import os
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
